d2b9445fe262c188d3aee2cbdfc50f83f1aa0f19
Dave Syer <dsyer@vmware.com>
2021-11-08 16:42:37 +0000

    Switch to HTMX

--- src/main/java/com/example/jsdemo/JsDemoApplication.java
+++ src/main/java/com/example/jsdemo/JsDemoApplication.java
@@ -1,8 +1,6 @@
 package com.example.jsdemo;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +8,12 @@ import org.springframework.http.MediaType;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -21,19 +22,23 @@ import reactor.core.publisher.Flux;
 public class JsDemoApplication {
 
 	@GetMapping("/user")
-	public Map<String, Object> user() {
-		return Collections.singletonMap("name", "Fred");
+	public String user() {
+		return "Fred";
+	}
+
+	@PostMapping("/greet")
+	public Mono<String> greet(ServerWebExchange request) {
+		return request.getFormData().map(map -> "Hello " + map.getFirst("value") + "!");
+	}
+
+	@GetMapping("/time")
+	public String time() {
+		return "Time: " + System.currentTimeMillis();
 	}
 
 	@GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<String> stream() {
-		return Flux.interval(Duration.ofSeconds(2)).map(value -> event(value));
-	}
-
-	private String event(long value) {
-		return "<turbo-stream action=\"append\" target=\"load\"><template><div>" //
-				+ "Index: " + value + " Time: " + System.currentTimeMillis() //
-				+ "</div></temlate></turbo-stream>";
+		return Flux.interval(Duration.ofSeconds(20)).map(value -> value.toString());
 	}
 
 	public static void main(String[] args) {
