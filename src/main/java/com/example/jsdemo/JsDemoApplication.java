c4aba0fac845e25daec69423a0a74ac64ae09765
Dave Syer <dsyer@vmware.com>
2021-11-09 17:45:30 +0000

    Support for Flux of Rendering

--- src/main/java/com/example/jsdemo/JsDemoApplication.java
+++ src/main/java/com/example/jsdemo/JsDemoApplication.java
@@ -1,21 +1,34 @@
 package com.example.jsdemo;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
-import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.HandlerResult;
+import org.springframework.web.reactive.HandlerResultHandler;
+import org.springframework.web.reactive.result.method.InvocableHandlerMethod;
+import org.springframework.web.reactive.result.view.Rendering;
+import org.springframework.web.reactive.result.view.View;
+import org.springframework.web.reactive.result.view.ViewResolver;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.ServerWebExchangeDecorator;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -46,6 +59,12 @@ public class JsDemoApplication {
 		return Flux.interval(Duration.ofSeconds(20)).map(value -> value.toString());
 	}
 
+	@GetMapping(path = "/test")
+	public Flux<Rendering> test() {
+		return Flux.just(Rendering.view("test").modelAttribute("value", "Test").build(),
+				Rendering.view("test").modelAttribute("value", "Foo").build());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(JsDemoApplication.class, args);
 	}
@@ -60,6 +79,106 @@ public class JsDemoApplication {
 		public void setValue(String value) {
 			this.value = value;
 		}
+	}
+
+}
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CompositeViewRenderer implements HandlerResultHandler {
+
+	private final ViewResolver resolver;
+
+	public CompositeViewRenderer(ViewResolver resolver) {
+		this.resolver = resolver;
+	}
+
+	@Override
+	public boolean supports(HandlerResult result) {
+		if (Publisher.class.isAssignableFrom(result.getReturnType().toClass())) {
+			if (Rendering.class.isAssignableFrom(result.getReturnType().getGeneric(0).toClass())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Mono<Void> handleResult(ServerWebExchange exchange, HandlerResult result) {
+		String[] methodAnnotation = ((InvocableHandlerMethod) result.getHandler())
+				.getMethodAnnotation(RequestMapping.class).produces();
+		MediaType type = methodAnnotation.length > 0 ? MediaType.valueOf(methodAnnotation[0]) : MediaType.TEXT_HTML;
+		exchange.getResponse().getHeaders().setContentType(type);
+		boolean sse = MediaType.TEXT_EVENT_STREAM.includes(type);
+		@SuppressWarnings("unchecked")
+		Flux<Rendering> renderings = Flux.from((Publisher<Rendering>) result.getReturnValue());
+		final ServerWebExchange wrapper = new ExchangeWrapper(exchange);
+		return render(wrapper, renderings, sse).then(Mono.defer(
+				() -> exchange.getResponse().writeAndFlushWith(((ResponseWrapper) wrapper.getResponse()).getBody())));
+	}
+
+	private Mono<Void> render(ServerWebExchange exchange, Flux<Rendering> renderings, boolean sse) {
+		return renderings.map(rendering -> render(exchange, rendering, sse))
+				.flatMap(thing -> thing.concatWith(closer(exchange))).then();
+	}
+
+	private Mono<Void> closer(ServerWebExchange exchange) {
+		DataBuffer buffer = exchange.getResponse().bufferFactory().allocateBuffer();
+		buffer.write("\n\n", StandardCharsets.UTF_8);
+		return exchange.getResponse().writeWith(Mono.just(buffer));
+	}
+
+	private Mono<Void> render(ServerWebExchange exchange, Rendering rendering, boolean sse) {
+		Mono<View> view = null;
+		if (rendering.view() instanceof View) {
+			view = Mono.just((View) rendering.view());
+		} else {
+			view = resolver.resolveViewName((String) rendering.view(), exchange.getLocaleContext().getLocale());
+		}
+		// TODO: something with sse to insert data: tags
+		return view.flatMap(actual -> actual.render(rendering.modelAttributes(), null, exchange));
+	}
+
+	static class ExchangeWrapper extends ServerWebExchangeDecorator {
+
+		private ServerHttpResponse response;
+
+		protected ExchangeWrapper(ServerWebExchange delegate) {
+			super(delegate);
+			this.response = new ResponseWrapper(super.getResponse());
+		}
+
+		@Override
+		public ServerHttpResponse getResponse() {
+			return this.response;
+		}
+
+	}
+
+	static class ResponseWrapper extends ServerHttpResponseDecorator {
+
+		private Flux<Flux<DataBuffer>> body = Flux.empty();
+
+		public Flux<Flux<DataBuffer>> getBody() {
+			return body;
+		}
+
+		public ResponseWrapper(ServerHttpResponse delegate) {
+			super(delegate);
+		}
+
+		@Override
+		public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+			return writeAndFlushWith(Mono.just(body));
+		}
+
+		@Override
+		public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
+			Flux<Flux<DataBuffer>> map = Flux.from(body).map(publisher -> Flux.from(publisher));
+			this.body = this.body.concatWith(map);
+			return Mono.empty();
+		}
+
 	}
 
 }
