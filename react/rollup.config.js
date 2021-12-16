21ffd71a956d49ebcdb11b382a578f0c3a8a4a7f
Dave Syer <dsyer@vmware.com>
2021-12-16 10:22:57 +0000

    Add notes on React with Rollup

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -1,6 +1,9 @@
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import resolve from "@rollup/plugin-node-resolve";
 import scss from "rollup-plugin-scss";
 import postcss from "rollup-plugin-postcss";
+import { babel } from '@rollup/plugin-babel';
 
 export default {
   input: "src/main/js/index.js",
@@ -9,6 +12,12 @@ export default {
     format: "esm",
   },
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      'preventAssignment': true
+    }),
+    babel(),
+    commonjs(),
     resolve({
       esm: true,
       main: true,
--- react/src/main/js/index.js
+++ react/src/main/js/index.js
@@ -2,33 +2,23 @@ import './style.scss';
 import '@springio/utils/style.css';
 
 import 'bootstrap';
-import * as Turbo from '@hotwired/turbo';
 import '@springio/utils';
-import { Application, Controller } from '@hotwired/stimulus';
 import { Chart, BarController, BarElement, PieController, ArcElement, LinearScale, CategoryScale, Title, Legend } from 'chart.js';
-
-Turbo.connectStreamSource(new EventSource("/stream"))
-window.Stimulus = Application.start();
 
 Chart.register(BarController, BarElement, PieController, ArcElement, LinearScale, CategoryScale, Title, Legend);
 
-Stimulus.register("hello", class extends Controller {
-	static targets = ["name", "output", "auth"]
-	initialize() {
-		let hello = this;
-		fetch("/user").then(response => {
-			response.json().then(data => {
-				hello.authTarget.textContent = `Logged in as: ${data.name}`;
-			});
-		});
-	}
-	greet() {
-		this.outputTarget.textContent = `Hello, ${this.nameTarget.value}!`;
-	};
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-Stimulus.register("chart", class extends Controller {
-	static targets = ["canvas"]
+class ChartChooser extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.clear = this.clear.bind(this);
+		this.bar = this.bar.bind(this);
+		this.pie = this.pie.bind(this);
+		this.doughnut = this.doughnut.bind(this);
+	};
 	pops(type, legend) {
 		let chart = this;
 		this.clear();
@@ -36,13 +26,13 @@ Stimulus.register("chart", class extends Controller {
 			response.json().then(data => {
 				data.type = type;
 				data.options.plugins.legend.display = legend;
-				chart.active = new Chart(chart.canvasTarget, data);
+				chart.setState({ active: new Chart(document.getElementById("canvas"), data) });
 			});
-		});;
+		});
 	};
 	clear() {
-		if (this.active) {
-			this.active.destroy();
+		if (this.state.active) {
+			this.state.active.destroy();
 		}
 	};
 	bar() {
@@ -54,5 +44,93 @@ Stimulus.register("chart", class extends Controller {
 	doughnut() {
 		this.pops("doughnut", true);
 	};
-});
+	render() {
+		return <div>
+			<button className="btn btn-primary" onClick={this.clear}>Clear</button>
+			<button className="btn btn-primary" onClick={this.bar}>Bar</button>
+			<button className="btn btn-primary" onClick={this.pie}>Pie</button>
+			<button className="btn btn-primary" onClick={this.doughnut}>Doughnut</button>
+		</div>;
+	}
+}
+ReactDOM.render(
+	<ChartChooser />,
+	document.getElementById('chooser')
+);
 
+class Auth extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { user: 'Unauthenticated' };
+	};
+	componentDidMount() {
+		let hello = this;
+		fetch("/user").then(response => {
+			response.json().then(data => {
+				hello.setState({ user: `Logged in as: ${data.name}` });
+			});
+		});
+	};
+	render() {
+		return <div id="auth">{this.state.user}</div>;
+	}
+};
+class Hello extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { name: '', message: '' };
+		this.greet = this.greet.bind(this);
+		this.change = this.change.bind(this);
+	};
+	greet() {
+		this.setState({ message: `Hello ${this.state.name}!` })
+	}
+	change(event) {
+		this.setState({ name: event.target.value })
+	}
+	render() {
+		return <div>
+			<div id="greeting">{this.state.message}</div>
+			<input id="name" name="value" type="text" value={this.state.name} onChange={this.change} />
+			<button className="btn btn-primary" onClick={this.greet}>Greet</button>
+		</div>;
+	}
+}
+ReactDOM.render(
+	<div className="container" id="hello">
+		<Auth />
+		<Hello />
+	</div>,
+	document.getElementById('hello')
+);
+
+class Content extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { html: '' };
+		this.fetch = this.fetch.bind(this);
+	};
+	fetch() {
+		let hello = this;
+		fetch("/test").then(response => {
+			response.text().then(data => {
+				hello.setState({ html: data });
+			});
+		});
+	}
+	render() {
+		return <div>
+			<div dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+			<button className="btn btn-primary" onClick={this.fetch}>Fetch</button>
+		</div>;
+	}
+}
+ReactDOM.render(
+	<Content />,
+	document.getElementById('root')
+);
+
+var events = new EventSource("/stream");
+events.onmessage = e => {
+	document.getElementById("load").innerHTML = e.data;
+}
